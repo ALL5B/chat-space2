d5bca2145a102ad676d0a58d3c691e745fb9c036
ALL5B <kugen.1018@gmail.com>
2018-12-05 21:10:48 +0900

    指摘箇所修正しました。
    htmlの共通部分が多いのでimageがある場合追加するようにしました。
    メッセージに更新があれば更新するようにしました。
    失敗時の記述もしました。

--- app/assets/javascripts/automatic-update.js
+++ app/assets/javascripts/automatic-update.js
@@ -2,32 +2,27 @@ $(function(){
 
 
   function updateHtml(message){
-    if(message.image.url){
     var html = `<p class="chat__message__user">
                   ${message.name}
                   <span class="chat__message__user__created-at">
                     ${message.created_at}
                   </span>
                 </p>
-                <img width="200" height="200" src="${message.image.url}">
+                `
+                if(message.image.url){
+                  `
+                  <img width="200" height="200" src="${message.image.url}">
+                  `
+                }
+                `
                 <p class="chat__message__text">
                   ${message.content}
-                </p>`
-    }else{
-      var html = `<p class="chat__message__user">
-                  ${message.name}
-                  <span class="chat__message__user__created-at">
-                    ${message.created_at}
-                  </span>
-                </p>
-                <p class="chat__message__text">
-                  ${message.content}
-                </p>`
-    }
+                </p>`;
     return html;
   }
 
   setInterval(function(){
+    var messageCount = $('.chat__message__user').length;
     $.ajax({
       type: 'GET',
       url: location.href,
@@ -35,12 +30,17 @@ $(function(){
     })
 
     .done(function(data){
+      var updateCount = data.messages.length;
       var insertHtml = '';
-      data.messages.forEach(function(message){
-        insertHtml += updateHtml(message)
-      });
-      $('chat__message__text').html(insertHtml)
+      if(updateCount > messageCount){
+        data.messages.forEach(function(message){
+          insertHtml += updateHtml(message)
+        });
+        $('chat__message__text').html(insertHtml)
+      }
     })
-
+    .fail(function(){
+      alert('自動更新に失敗しました');
+    })
   },5000);
 });
