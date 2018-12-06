e78391cf50261e02ad76c30a14e5b4f88b32fe68
ALL5B <kugen.1018@gmail.com>
2018-12-06 16:58:04 +0900

    自動更新できるよう修正

--- app/assets/javascripts/automatic-update.js
+++ app/assets/javascripts/automatic-update.js
@@ -2,45 +2,52 @@ $(function(){
 
 
   function updateHtml(message){
-    var html = `<p class="chat__message__user">
-                  ${message.name}
-                  <span class="chat__message__user__created-at">
-                    ${message.created_at}
-                  </span>
-                </p>
-                `
-                if(message.image.url){
-                  `
-                  <img width="200" height="200" src="${message.image.url}">
-                  `
-                }
-                `
-                <p class="chat__message__text">
-                  ${message.content}
-                </p>`;
-    return html;
+    var html = ""
+    var user = `<p class="chat__message__user" message-id="${message.id}">
+                    ${message.name}
+                    <span class="chat__message__user__created-at">
+                      ${message.date}
+                    </span>
+                  </p>`;
+    var img = `<img width="200" height="200" src="${message.image.url}">`;
+    var content = `<p class="chat__message__text">
+                    ${message.content}
+                    </p>`;
+
+    html += user
+    if (message.image.url){
+      html += img
+    }
+    html += content
+
+      return html;
   }
 
   setInterval(function(){
-    var messageCount = $('.chat__message__user').length;
-    $.ajax({
-      type: 'GET',
-      url: location.href,
-      dataType: 'json'
-    })
+    // documentのロード完了後に
+    $(function() {
+      var messageUsers = $('.chat__message__user') ;
+      var currentMessageId = messageUsers.last().attr('message-id');
 
-    .done(function(data){
-      var updateCount = data.messages.length;
-      var insertHtml = '';
-      if(updateCount > messageCount){
-        data.messages.forEach(function(message){
-          insertHtml += updateHtml(message)
-        });
-        $('chat__message__text').html(insertHtml)
-      }
-    })
-    .fail(function(){
-      alert('自動更新に失敗しました');
-    })
+      $.ajax({
+        type: 'GET',
+        url: location.href,
+        data: {messageid: currentMessageId},
+        dataType: 'json'
+      })
+
+      .done(function(data){
+        var insertHtml = '';
+        if(data.messages){
+          data.messages.forEach(function(message){
+            insertHtml += updateHtml(message)
+          });
+          $('.chat__message__text').last().append(insertHtml)
+        }
+      })
+      .fail(function(){
+        alert('自動更新に失敗しました');
+      })
+    });
   },5000);
 });
